a3de43074de390047e16fbfde697f38cc97c7a69
felipeMarroquinTepale <felipe.01tepa@gmail.com>
2021-12-23 11:14:19 -0600

    update page home

--- app/pages/Home.js
+++ app/pages/Home.js
@@ -4,6 +4,7 @@ import '/app/assets/stylesheets/Home.css'
 import Card from "../components/card";
 import Footer from "../components/Footer";
 import Navbar from "../components/Header";
+import "../assets/stylesheets/cardHome.css"
 
 
 class Home extends React.Component{
@@ -15,10 +16,13 @@ class Home extends React.Component{
                 <div className="tituloreseña">
                     <h4 className="text-black"><span className="BIT">BIT</span>ZERO SE PREOCUPA POR TI</h4>
                 </div>
+                <div className="container" >
                 <div className="reseña">
-                    <p>La vision de nuestros clientes es nuestra mejor practica que podemos usar para poder generar los resultados esperados.<br/>
-                        Mas de N años de experiencia que nos respaldan nos ha llevado a perfeccionar el uso de las mejores practicas para producir <br/>y mantener software de calidad a nuestros clientes.
+                    <p>La vision de nuestros clientes es nuestra mejor practica que podemos usar para poder generar los resultados esperados.
+                        Mas de N años de experiencia que nos respaldan nos ha llevado a perfeccionar el uso de las mejores practicas para producir
+                        y mantener software de calidad a nuestros clientes.
                     </p>
+                </div>
                 </div>
                 <Card/>
                 <Footer/>
